a3b36380967ad7d483075e6cb16a23234685e30a
harshaTejaR <40051202+harshaTejaR@users.noreply.github.com>
2022-07-29 23:07:10 +0530

    calculator UI part done functionality to be implimented

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import React from "react";
 import "./App.css";
 function App() {
 
+
   const createDigits = () => {
     const digits = [];
-    for(let i =1; i<10; i++){
-      digits.push(<button key={i}>{i}</button>)
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}> {i} </button>);
     }
     return digits;
-  }
+  };
 
   return (
     <div className="App">
-      <div className="display">
-        <span>Display</span>
-      </div>
-      <div className="operators">
-        <button>/</button>
-        <button>x</button>
-        <button>+</button>        
-        <button>-</button>
-        <button>DEL</button>
-      </div>
-      <div className="digits">
-        {createDigits()}
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+      <div className="calculator">
+        <div className="display">
+          <span>{0}</span>
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>x</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
     </div>
   );
